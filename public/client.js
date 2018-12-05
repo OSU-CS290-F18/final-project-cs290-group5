73d7959b908be05f53e41fd75519ff3c9b13c20f
Zander Work <zander@zanderwork.com>
2018-12-05 14:51:13 -0800

    started client-side js rebuild

--- public/client.js
+++ public/client.js
@@ -1,32 +1,101 @@
+// Vars for new user modal on page load
+let newUserModal = document.getElementById("new-user-modal");
+let newUserSubmit = document.getElementById("new-user-submit");
+let newUserField = document.getElementById("new-user-field");
 
-var sendButton = document.getElementById('send');
-var message = document.getElementById("text-input");
-var newUserButton = document.getElementById("new-user");
-var startButton = document.getElementById("start");
-var userName = document.getElementById("user-input");
-var window = document.getElementById("chat-input");
-//var newChannelButton = document.getElementById()
-//var userName = document.getElementById("user-input");
-/* var chat = io.of('chat');
-var user = io.of('users');
-var channel = io.of('channels'); */
-//document.getElementById('backdrop').classList.toggle("hidden");
+// Vars for new channel modal
+let newChannelModal = document.getElementById("new-channel-modal");
+let newChannelSubmit = document.getElementById("new-channel-submit");
+let newChannelAbort = document.getElementById("new-channel-abort");
+let newChannelField = document.getElementById("new-channel-field");
 
-newUserButton.addEventListener('click', ()=>
-{
+// Modal backdrop var
+let modalBackrdop = document.getElementById("modal-backdrop");
+
+// Vars for sending a message
+let messageTextBox = document.getElementById("message-text-box");
+let messageSend = document.getElementById("message-send");
+
+// Vars for channel & message list
+let channelList = document.getElementById("channels-list");
+let messageList = document.getElementById("message-list");
+
+// Socket.io handle
+const socket = io();
+
+// Data (both are strings)
+let username, activeChannel;
+
+// Clear all rendered messages
+function clearMessages() {
+    while (messageList.firstChild) {
+        messageList.removeChild(messageList.firstChild);
+    }
+}
+
+// Render new message
+function renderMessage(user, msg) {
+    let msgItem = document.createElement("li");
+    msgItem.appendChild(document.createTextNode(msg));
+    //TODO add proper attributes
+    //TODO do something with the user, talk with Paulina
+    messageList.appendChild(msgItem);
+}
+
+// Render new channel in sidebar
+function addChannelToSidebar(name) {
+    let channelItem = document.createElement("li");
+    channelItem.appendChild(document.createTextNode(name));
+    //TODO add proper attributes
+    channelList.appendChild(channelItem);
+}
+
+// Set active channel
+function setActiveChannel(name) {
+    // get element for new active channel in sidebar
+    let channelsInList = channelList.getElementsByTagName("li");
+    for (let i = 0; i < channelsInList.length; i++) {
+        if (name === channelsInList[i].textContent.valueOf()) {
+            // got new active channel
+            //TODO do something with it
+        } else {
+            //TODO set channel not active in CSS
+        }
+    }
+    activeChannel = name;
+}
+
+// Handler for setting username
+newUserSubmit.addEventListener("click", () => {
+    // get field value
+    username = newUserField.value;
+    console.log(`got username: ${username}`);
+
+    // close modal
+    
+});
+
+// OLD CODE
+
+let sendButton = document.getElementById('send');
+let message = document.getElementById("text-input");
+let newUserButton = document.getElementById("new-user");
+let startButton = document.getElementById("start");
+let userName = document.getElementById("user-input");
+let window = document.getElementById("chat-input");
+
+newUserButton.addEventListener('click', () => {
     document.getElementById('backdrop').classList.toggle("hidden");
     document.getElementById('user-popup').classList.toggle("hidden");
 });
 
-startButton.addEventListener('click', ()=>
-{
-    if(userName.value = ""){
+startButton.addEventListener('click', () => {
+    if (userName.value == "") {
         alert("Please enter username");
-    }else{
-        
-        var popUpContainer = document.createElement("div");
+    } else {
+        let popUpContainer = document.createElement("div");
         popUpContainer.classList.add("inside-popup");
-        var userNameContainer = document.createElement("div");
+        let userNameContainer = document.createElement("div");
         userNameContainer.textContent =  userName.value;
         userNameContainer.classList.add("username");
         popUpContainer.appendChild(userNameContainer);
@@ -34,21 +103,15 @@ startButton.addEventListener('click', ()=>
         alert("new user added! ", userName.value);
         console.log(userName.value);
     }
-
     document.getElementById('backdrop').classList.toggle("hidden");
     document.getElementById('user-popup').classList.toggle("hidden");
-
 });
 
-
-
-sendButton.addEventListener('click', ()=>
-{
-    var messageContainer = document.createElement('div');
+sendButton.addEventListener('click', () => {
+    let messageContainer = document.createElement('div');
     messageContainer.classList.add("chat-input");
     messageContainer.textContent = message.value;
-    var messageContents = document.getElementsByClassName("chat-and-send");
+    let messageContents = document.getElementsByClassName("chat-and-send");
     messageContents[0].appendChild(messageContainer);
     message.value = "";
 }); 
-
