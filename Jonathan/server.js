47a0c13a3d541fffc822bda57f607a5e9f243c67
JonathanKoning <43650361+JonathanKoning@users.noreply.github.com>
2018-12-04 17:36:49 -0800

    updated server.js and edited index.html

--- Jonathan/server.js
+++ Jonathan/server.js
@@ -5,7 +5,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-
 //get the files from public folder
 app.use(express.static('public'));
 
@@ -21,13 +20,7 @@ app.get("*", function (req, res, next){
     console.log("404 page called");
 });
 
-//grab pictures
-// app.get("/public/:userId/:photoId", function(req, res, next){
-//     res.status(200);
-//     res.sendFile("<html><body><h1>Photo " + req.params.photoId + " by " + req.params.userId + "</h1></body></html>")
-// });
-
-//alert when user connects/disconnects to server-
+//alert when user connects/disconnects to server
 io.on('connection', function(socket){
     var addedUser = false;
     console.log('a user connected');
