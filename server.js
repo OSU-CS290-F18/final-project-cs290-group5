92cfdcccc7e266559cc7f84ce747c07507bf2075
Zander Work <zander@zanderwork.com>
2018-12-05 13:18:49 -0800

    express routing done

--- server.js
+++ server.js
@@ -18,14 +18,8 @@ const messages = new Messages(dao);
 //get the files from public folder
 app.use(express.static('public'));
 
-// app.get("/", function(req, res, next){
-//     res.status(200);
-//     res.sendFile(__dirname + '/public/index.html');
-// });
-
 //get error page
 app.get("*", function (req, res, next){
-    console.log("Got a request for ", req.url);
     res.status(404);
     res.sendFile(__dirname + '/public/404.html'); //bring to 404 page
     console.log("404 page called");
