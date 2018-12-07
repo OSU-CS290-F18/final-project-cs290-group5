bc6ae532bf0b6d66f37146f6bc76361ac89f78ab
Zander Work <zander@zanderwork.com>
2018-12-06 21:17:50 -0800

    fixed bad var name

--- public/client.js
+++ public/client.js
@@ -148,7 +148,7 @@ messageSend.addEventListener("click", () => {
     console.log(`sending message: ${msgText}`);
 
     // broadcast
-    socket.emit("new message", newChannelName, msgText);
+    socket.emit("new message", activeChannel, msgText);
 });
 
 // Send disconnect on window unload
